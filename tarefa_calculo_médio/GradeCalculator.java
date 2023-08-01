d6b8db6c8cd1a9f65de594364dc9e266eb29904d
Ryan Seiyu <ryan_seiyu@hotmail.com>
2023-08-01 19:18:21 -0300

    calculadora de notas

---
+++ tarefa_calculo_médio/GradeCalculator.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+
+public class GradeCalculator {
+    public static void main(String[] args) {
+        Random random = new Random();
+
+        int grade1 = random.nextInt(11);
+        int grade2 = random.nextInt(11);
+        int grade3 = random.nextInt(11);
+        int grade4 = random.nextInt(11);
+
+        double average = (grade1 + grade2 + grade3 + grade4) / 4.0;
+        System.out.println("The average grade is: " + average);
+    }
+}
