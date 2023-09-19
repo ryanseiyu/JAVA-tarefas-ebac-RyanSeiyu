5fd2e069a5dfb5d91fd3cf396663dfd16ef045d7
Ryan Seiyu <ryan_seiyu@hotmail.com>
2023-09-19 17:43:03 -0300

    Tarefa Módulo 40

---
+++ Tarefa_Modulo40/src/main/java/com/rseiyu/springmaven/SpringmavenApplication.java
@@ -0,0 +1,13 @@
+package com.rseiyu.springmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmavenApplication.class, args);
+	}
+
+}
